2191eabca6bdd0f429f932f6a56a387976495d0c
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-03-12 23:36:08 +0800

    Bump project.clj version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jvm.tools.analyzer "0.3.0"
+(defproject jvm.tools.analyzer "0.3.1-SNAPSHOT"
   :description "Interface to Clojure Analyzer"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.trace "0.7.3"]])
