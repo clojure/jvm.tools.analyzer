1a03e052ac18006715130fc0b8fcc89849ce4061
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-02-04 17:22:12 +0800

    bump project.clj dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/jvm.tools.analyzer "0.6.0-SNAPSHOT"
+(defproject org.clojure/jvm.tools.analyzer "0.6.1-SNAPSHOT"
   :description "Interface to Clojure Analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
